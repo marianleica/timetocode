7911190a2306e2e7b7cac95c683f54c4d9dd24b8
marianleica <maleica@microsoft.com>
2024-06-07 15:54:54 +0300

    Adding converting string to int

---
+++ convertStringToInt/Program.cs
@@ -0,0 +1,13 @@
+﻿void ConvertStringToInt() {
+
+    string svalue = "123";
+    int value = 0;
+
+    foreach (char c in svalue){
+        value = value*10 + (c-'0');
+        Console.WriteLine($"{value},  {c}");
+    }
+
+    Console.WriteLine($"{value}");
+    
+}
