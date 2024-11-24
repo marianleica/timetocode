08093afb6104d62f53618263a628704f8764591b
marianleica <maleica@microsoft.com>
2024-11-24 20:39:24 +0200

    Added code

---
+++ palindromeCheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string input)
+    {
+        for (int i = 0, j = input.Length - 1; i < j; i++, j--)
+            if (Char.ToLower(input[i]) != Char.ToLower(input[j]))
+                return false;
+        return true;
+    }
+
+    static void Main()
+    {
+        string testWord = "Madam";
+        Console.WriteLine($"'{testWord}' is {(IsPalindrome(testWord) ? "a palindrome" : "not a palindrome")}.");        
+    }
+}
