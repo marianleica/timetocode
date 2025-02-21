f4312afef04b5d6f514d98b875d10d9134dab75a
marianleica <marianleica@lesformidables.club>
2025-02-21 21:41:03 +0200

    new problem

---
+++ fizzBuzzList/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+Constraints:
+
+1 <= n <= 104
+
+*/
+
+public class Program
+{
+    public static string[] FizzBuzz(int n)
+    {
+        string[] result = new string[n];
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                result[i - 1] = "FizzBuzz";
+            }
+            else if (i % 3 == 0)
+            {
+                result[i - 1] = "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                result[i - 1] = "Buzz";
+            }
+            else
+            {
+                result[i - 1] = i.ToString();
+            }
+        }
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = 15;
+        string[] result = FizzBuzz(n);
+
+        Console.Write("Output: [");
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.Write(result[i]);
+            if (i < result.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
