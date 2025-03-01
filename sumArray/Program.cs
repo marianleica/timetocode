5e4ba05aeb2eb6b36f48ae187a00fd361164bdce
marianleica <marianleica@lesformidables.club>
2025-03-01 11:39:00 +0200

    new problem

---
+++ sumArray/Program.cs
@@ -0,0 +1,25 @@
+﻿// function that calculates the sum of all elemnts in the array
+
+using System;
+
+namespace sumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 2, 3, 4, 5};
+            Console.WriteLine(sumArray(arr));
+        }
+
+        static int sumArray(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ tests/Program.cs
@@ -0,0 +1,28 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Choose number:");
+        int input = 7;
+        int sum = 0;
+        int i;
+        
+        int[] array = new int[input + 1];
+
+        for (i = 0; i < input; i++)
+        {
+            array[i] = i + 1;
+        }
+        
+        for (i = 0; i < array.Length; i++)
+        {
+            sum = sum + array[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
